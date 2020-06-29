c84e8e29249546f185bfebce1e31ad9f357756c6
lurise <yhdbk@163.com>
2020-06-29 23:28:17 +0800

    优化内容列表页面，超长显示省略号

--- src/view/content/contentlist/contentlist.vue
+++ src/view/content/contentlist/contentlist.vue
@@ -1,95 +1,292 @@
 <template>
-  <List item-layout="vertical" style="padding-left:30px;padding-top: 40px;padding-right: 200px">
-    <ListItem v-for="(item,index) in data" :key="item.title" @mouseover.native="onMouseOver(index)"
-              @mouseleave.native="onMouseLeave(index)">
-      <ListItemMeta :title="item.title"/>
-      {{ item.content }}
-      <template slot="action">
-        <li>
-          <p>更新时间</p>
-        </li>
-        <li>
-          <p>2020年6月23日</p>
-        </li>
-        <ul style="padding-top: 10px" v-if="mouseOver[index].show">
-          <li>
-            <Tooltip content="编辑">
-              <Icon type="ios-create-outline" size="24" @click="editContent(index)"/>
-            </Tooltip>
-            <Tooltip content="删除">
-              <Poptip trigger="hover" placement="bottom" confirm
-                      title="该操作将删除图文消息，确定删除该图文？"
-                      @on-ok="ok(index)"
-                      @on-cancel="cancel(index)">
-                <Icon type="md-trash" size="24" style="padding-left: 10px"/>
-              </Poptip>
-            </Tooltip>
-          </li>
-        </ul>
-
-      </template>
-      <template slot="extra">
-        <img src="https://dev-file.iviewui.com/5wxHCQMUyrauMCGSVEYVxHR5JmvS7DpH/large" style="width: 280px">
-      </template>
-    </ListItem>
-  </List>
+  <div>
+    <div class="search-con search-con-top">
+      <Select v-model="searchKey" class="search-col">
+        <Option v-for="item in options" :value="item" :key="`search-col-${item}`">
+          {{ item }}
+        </Option>
+      </Select>
+      <Input @on-change="handleClear" clearable placeholder="输入关键字搜索" class="search-input" v-model="searchValue"/>
+      <Button @click="handleSearch" class="search-btn" type="primary">
+        <Icon type="search"/>&nbsp;&nbsp;搜索
+      </Button>
+    </div>
+    <Scroll ref="Scroll" :on-reach-bottom="handleReachBottom" height="700">
+      <List item-layout="vertical" style="padding-left:30px;padding-top: 40px;padding-right: 200px" :loading="listLoading">
+        <ListItem v-for="(item,index) in data" :key="item.title" @mouseover.native="onMouseOver(index)"
+                  @mouseleave.native="onMouseLeave(index)">
+          <ListItemMeta :title="item.title"/>
+          <p v-html="item.content" class="itemContent"></p>
+          <template slot="action">
+            <li>
+              <p>更新时间</p>
+            </li>
+            <li>
+              <p>2020年6月23日</p>
+            </li>
+            <li>
+              <p>{{item.catgory}}</p>
+            </li>
+            <ul style="padding-top: 10px" v-if="mouseOver[index].show">
+              <li>
+                <Tooltip content="编辑">
+                  <Icon type="ios-create-outline" size="24" @click="editContent(index)"/>
+                </Tooltip>
+                <Tooltip content="删除">
+                  <Poptip trigger="hover" placement="bottom" confirm
+                          title="该操作将删除图文消息，确定删除该图文？"
+                          @on-ok="ok(index)"
+                          @on-cancel="cancel(index)">
+                    <Icon type="md-trash" size="24" style="padding-left: 10px"/>
+                  </Poptip>
+                </Tooltip>
+              </li>
+            </ul>
+          </template>
+          <template slot="extra">
+            <img src="https://dev-file.iviewui.com/5wxHCQMUyrauMCGSVEYVxHR5JmvS7DpH/large" style="width: 280px">
+          </template>
+        </ListItem>
+      </List>
+    </Scroll>
+    <Button type="primary" @click="loadMore">加载更多</Button>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'contentlist',
-  data () {
-    return {
-      mouseOver: [{
-        show: false
-      }, {
-        show: false
-      }, {
-        show: false
-      }],
-      data: [
-        {
-          title: 'This is title 1',
-          description: 'This is description, this is description, this is description.',
-          avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
-          content: 'This is the content, this is the content, this is the content, this is the content.'
-        },
-        {
-          title: 'This is title 2',
-          description: 'This is description, this is description, this is description.',
-          avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
-          content: 'This is the content, this is the content, this is the content, this is the content.'
-        },
-        {
-          title: 'This is title 3',
-          description: 'This is description, this is description, this is description.',
-          avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
-          content: 'This is the content, this is the content, this is the content, this is the content.'
-        }
-      ]
+  export default {
+    name: 'contentlist',
+    data() {
+      return {
+        listLoading:false,
+        searchKey: '',
+        options: ['标题', '内容'],
+        searchValue: '',
+        mouseOver: [{
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }, {
+          show: false
+        }],
+        data: [
+          {
+            title: 'This is title 1',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 2',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 3',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.<br>his is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the con<br>his is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the con<br>his is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the con<br>his is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the content.This is the content, this is the content, this is the content, this is the con',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 4',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 5',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 6',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 7',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 8',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 9',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 10',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 11',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          },
+          {
+            title: 'This is title 12',
+            description: 'This is description, this is description, this is description.',
+            avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+            content: 'This is the content, this is the content, this is the content, this is the content.',
+            catgory:'分类一'
+          }
+        ]
 
-    }
-  },
-  methods: {
-    onMouseOver (index) {
-      this.mouseOver[index].show = true
+      }
     },
-    onMouseLeave (index) {
-      this.mouseOver[index].show = false
-    },
-    ok (index) {
-      this.$Message.info('正在删除' + index)
-    },
-    cancel (index) {
+    methods: {
+      onMouseOver(index) {
+        this.mouseOver[index].show = true
+      },
+      onMouseLeave(index) {
+        this.mouseOver[index].show = false
+      },
+      ok(index) {
+        this.$Message.info('正在删除' + index)
+      },
+      cancel(index) {
 
-    },
-    editContent (index) {
-      this.$Message.info('正在编辑' + index)
+      },
+      editContent(index) {
+        this.$Message.info('正在编辑' + index)
+      },
+      handleClear(e) {
+        if (e.target.value === '') this.insideTableData = this.value
+      },
+      handleSearch() {
+        console.log('handle search')
+      },
+      handleReachBottom() {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            for (let i = 0; i < 10; i++) {
+              if (this.data.length > 40) {
+                this.$Message.info("已经加载完成全部数据了")
+                resolve();
+                return
+              }
+              let content = {
+                title: 'This is  added title ' + i,
+                description: 'This is description, this is description, this is description.',
+                avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+                content: 'This is the content, this is the content, this is the content, this is the content.',
+                catgory:'分类一'
+              }
+              this.data.push(content)
+              this.mouseOver.push({show: false})
+            }
+
+            resolve();
+          }, 2000)
+        })
+      },
+      loadMore() {
+        this.listLoading = true;
+        setTimeout(() => {
+
+          for (let i = 0; i < 10; i++) {
+            if (this.data.length > 40) {
+              this.$Message.info("已经加载完成全部数据了")
+              return
+            }
+            let content = {
+              title: 'This is  added title ' + i,
+              description: 'This is description, this is description, this is description.',
+              avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
+              content: 'This is the content, this is the content, this is the content, this is the content.',
+              catgory:'分类一'
+            }
+            this.data.push(content)
+            this.mouseOver.push({show: false})
+
+          }
+
+          this.listLoading = false;
+        }, 2000)
+      }
     }
   }
-}
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .search-con {
+    padding: 10px 0;
 
+    .search {
+      &-col {
+        display: inline-block;
+        width: 200px;
+      }
+
+      &-input {
+        display: inline-block;
+        width: 200px;
+        margin-left: 2px;
+      }
+
+      &-btn {
+        margin-left: 2px;
+      }
+    }
+  }
+
+  .btns {
+    margin: auto 20px 10px auto;
+  }
+
+  .itemContent{
+    width: 500px;
+    display: block;
+    text-overflow: ellipsis;  /*超出内容用省略号*/
+    overflow: hidden; /*内容超出后隐藏*/
+    white-space: nowrap; /*文本不进行换行*/
+    height: 20px;
+    /*white-space: pre-line;*/
+  }
 </style>
--- src/view/template/templist.vue
+++ src/view/template/templist.vue
@@ -1,102 +1,104 @@
 <template>
-  <Row style="padding: 20px">
-    <Col span="6" v-for="(item,index) in tempData" :key="index">
-      <Card
-        style="width:275px;height:275px;padding:20px"
-        :shadow="true"
-        @mouseover.native="onMouseOver(index)"
-        @mouseleave.native="onMouseleave(index)">
-        <p slot="title" style="size: 50px;">{{item.tytle}}</p>
-        <p v-html="item.content"></p>
-        <p slot="extra">
-          <template v-if="mouseOver[index].show">
-            <Tooltip content="编辑" placement="top">
-              <Icon type="ios-create-outline" size="24" @click="editContent(index)"/>
-            </Tooltip>
-            <Tooltip content="删除" placement="top">
-              <Poptip trigger="hover" placement="bottom" confirm
-                      title="删除后不会影响已群发的图文消息，确定删除该模版？"
-                      @on-ok="ok(index)"
-                      @on-cancel="cancel(index)">
-                <Icon type="md-trash" size="24"/>
-              </Poptip>
-            </Tooltip>
-          </template>
-        </p>
-      </Card>
-    </Col>
-    <!--    <Col span="8">-->
-    <!--      <Card-->
-    <!--        style="width: 275px;height: 275px;padding: 20px"-->
-    <!--        :shadow="true"-->
-    <!--        @mouseover.native="onMouseOver(3)"-->
-    <!--        @mouseleave.native="onMouseleave(3)">-->
-    <!--        <p slot="title" style="size: 50px;">测试</p>-->
-    <!--        &lt;!&ndash;          {{item.content}}&ndash;&gt;-->
-    <!--        <p>test</p>-->
-    <!--        <p slot="extra">-->
-    <!--          <template v-if="mouseOver[1].show">-->
-    <!--            <Poptip trigger="hover" placement="bottom" content="编辑" style="text-align: center" width="50px"-->
-    <!--                    @click.native="editContent(1)" popper-class="edit-poppip">-->
-    <!--              <Icon type="ios-create-outline" size="24"/>-->
-    <!--            </Poptip>-->
-    <!--            <Poptip trigger="hover" placement="bottom" confirm-->
-    <!--                    title="删除后不会影响已群发的图文消息，确定删除该模版？"-->
-    <!--                    @on-ok="ok(1)"-->
-    <!--                    @on-cancel="cancel(1)" popper-class="delete-poppip">-->
-    <!--              <Icon type="md-trash" size="24"/>-->
-    <!--            </Poptip>-->
-    <!--          </template>-->
-    <!--        </p>-->
-    <!--      </Card>-->
-    <!--    </Col>-->
-  </Row>
+  <div>
 
+    <Row style="padding: 20px">
+      <Col span="6" v-for="(item,index) in tempData" :key="index">
+        <Card
+          style="width:275px;height:275px;padding:20px"
+          :shadow="true"
+          @mouseover.native="onMouseOver(index)"
+          @mouseleave.native="onMouseleave(index)">
+          <p slot="title" style="size: 50px;">{{item.tytle}}</p>
+          <p v-html="item.content"></p>
+          <p slot="extra">
+            <template v-if="mouseOver[index].show">
+              <Tooltip content="编辑" placement="top">
+                <Icon type="ios-create-outline" size="24" @click="editContent(index)"/>
+              </Tooltip>
+              <Tooltip content="删除" placement="top">
+                <Poptip trigger="hover" placement="bottom" confirm
+                        title="删除后不会影响已群发的图文消息，确定删除该模版？"
+                        @on-ok="ok(index)"
+                        @on-cancel="cancel(index)">
+                  <Icon type="md-trash" size="24"/>
+                </Poptip>
+              </Tooltip>
+            </template>
+          </p>
+        </Card>
+      </Col>
+      <!--    <Col span="8">-->
+      <!--      <Card-->
+      <!--        style="width: 275px;height: 275px;padding: 20px"-->
+      <!--        :shadow="true"-->
+      <!--        @mouseover.native="onMouseOver(3)"-->
+      <!--        @mouseleave.native="onMouseleave(3)">-->
+      <!--        <p slot="title" style="size: 50px;">测试</p>-->
+      <!--        &lt;!&ndash;          {{item.content}}&ndash;&gt;-->
+      <!--        <p>test</p>-->
+      <!--        <p slot="extra">-->
+      <!--          <template v-if="mouseOver[1].show">-->
+      <!--            <Poptip trigger="hover" placement="bottom" content="编辑" style="text-align: center" width="50px"-->
+      <!--                    @click.native="editContent(1)" popper-class="edit-poppip">-->
+      <!--              <Icon type="ios-create-outline" size="24"/>-->
+      <!--            </Poptip>-->
+      <!--            <Poptip trigger="hover" placement="bottom" confirm-->
+      <!--                    title="删除后不会影响已群发的图文消息，确定删除该模版？"-->
+      <!--                    @on-ok="ok(1)"-->
+      <!--                    @on-cancel="cancel(1)" popper-class="delete-poppip">-->
+      <!--              <Icon type="md-trash" size="24"/>-->
+      <!--            </Poptip>-->
+      <!--          </template>-->
+      <!--        </p>-->
+      <!--      </Card>-->
+      <!--    </Col>-->
+    </Row>
+  </div>
 </template>
 
 <script>
-import Tables from '_c/tables'
-import { getTemlData } from '../../api/data'
+  import Tables from '_c/tables'
+  import {getTemlData} from '../../api/data'
 
-export default {
-  name: 'templist',
-  data () {
-    return {
-      templateLoading: false,
-      tempData: [],
-      mouseOver: []
-    }
-  },
-  components: {
-    Tables
-  },
-  methods: {
-    onMouseOver (index) {
-      this.mouseOver[index].show = true
-    },
-    onMouseleave (index) {
-      this.mouseOver[index].show = false
-    },
-    ok (index) {
-
-    },
-    cancel (index) {
-
-    },
-    editContent (index) {
-      this.$Message.info('正在编辑' + index)
-    }
-  },
-  mounted () {
-    getTemlData().then(req => {
-      this.tempData = req.data
-      for (let i = 0; i < this.tempData.length; i++) {
-        let obj = { show: false }
-        this.mouseOver.push(obj)
+  export default {
+    name: 'templist',
+    data() {
+      return {
+        templateLoading: false,
+        tempData: [],
+        mouseOver: []
       }
-    })
+    },
+    components: {
+      Tables
+    },
+    methods: {
+      onMouseOver(index) {
+        this.mouseOver[index].show = true
+      },
+      onMouseleave(index) {
+        this.mouseOver[index].show = false
+      },
+      ok(index) {
+
+      },
+      cancel(index) {
+
+      },
+      editContent(index) {
+        this.$Message.info('正在编辑' + index)
+      }
+    },
+    mounted() {
+      getTemlData().then(req => {
+        this.tempData = req.data
+        for (let i = 0; i < this.tempData.length; i++) {
+          let obj = {show: false}
+          this.mouseOver.push(obj)
+        }
+      })
+    }
   }
-}
 </script>
 
 <style scoped>
